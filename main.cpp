3b8e65ff77e403471589a927e1e1a4bdb018613b
Aloïs Sebrechts <alois@Aloiss-MacBook-Pro.local>
2022-04-18 17:47:03 +0200

    enable_if

--- main.cpp
+++ main.cpp
@@ -6,13 +6,30 @@
 /*   By: asebrech <asebrech@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 13:00:22 by asebrech          #+#    #+#             */
-/*   Updated: 2022/04/14 13:00:55 by asebrech         ###   ########.fr       */
+/*   Updated: 2022/04/18 17:40:33 by asebrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "vector.hpp"
+#include "vector.hpp"
+
+#include <iostream>
+#include <vector>
+#include <string>
 
 int	main()
 {
-	ft::vector<int>	test;
+	std::vector<int>	vector(2, 117);
+
+	vector.push_back(10);
+	vector.push_back(117);
+	vector.push_back(66);
+
+	ft::vector<int>	ft_vector(vector.begin(), vector.end());
+	ft::vector<int>	ft_vector1(2, 100);
+
+	std::cout << ft_vector.data()[1] << std::endl;
+	std::cout << ft_vector1.data()[1] << std::endl;
+
+	std::cout << vector.max_size() << std::endl;
+	std::cout << ft_vector.max_size() << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: asebrech <asebrech@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 12:51:49 by asebrech          #+#    #+#             */
-/*   Updated: 2022/04/14 13:01:26 by asebrech         ###   ########.fr       */
+/*   Updated: 2022/04/18 17:40:51 by asebrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,102 @@
 # define VECTOR_HPP
 
 # include <memory>
+# include <stdexcept>
+# include <iterator>
+
+# include <iostream>
 
 namespace	ft
 {
+
+	template<bool Condition, typename T = void>
+	struct	enable_if {};
+
+	template<typename T>
+	struct	enable_if<true, T>
+	{
+		typedef T	type;
+	};
+
+
 	template < class T, class Alloc = std::allocator<T> >
 	class	vector
 	{
 		public :
 
-			typedef	T	value_type;
-			typedef	Alloc	allocator_type;
+			typedef T	value_type;
+			typedef Alloc	allocator_type;
+			typedef typename allocator_type::size_type	size_type;
+			typedef typename allocator_type::pointer	pointer;
+			typedef typename allocator_type::const_pointer	const_pointer;
 
 			explicit vector(const allocator_type & alloc = allocator_type()) : _alloc(alloc) {};
+			
+			explicit vector(size_type n, const value_type & val = value_type(),
+					const allocator_type & alloc = allocator_type()) : _alloc(alloc), _capacity(n), _size(n)
+			{
+				if (n > this->max_size())
+					throw	std::length_error("vector");
+				this->_data = this->_alloc.allocate(n);
+				for (size_type i = 0; i < n; i++)
+				{
+					this->_alloc.construct((this->_data + i), val);
+				}
+			}
+			
+			template <class	InputIterator > 
+			vector(InputIterator first, InputIterator last, const allocator_type & alloc = allocator_type(),
+				typename ft::enable_if<!std::is_integral<InputIterator>::value, std::nullptr_t>::type = nullptr) : _alloc(alloc)
+			{
+				this->_capacity = std::distance(first, last);
+				this->_size = this->_capacity;
+				if (this->_capacity > this->max_size())
+					throw	std::length_error("vector");
+				this->_data = this->_alloc.allocate(this->_capacity);
+				for (size_type i = 0; i < this->_capacity; i++)
+				{
+					this->_alloc.construct((this->_data + i), *first);
+					first++;
+				}
+			}
+
 			~vector() {};
+
+
+			pointer	data(void)
+			{
+				return (this->_data);
+			}
+
+			const_pointer	data(void) const
+			{
+				return (static_cast<const_pointer>(this->_data));
+			}
+
+			size_type	max_size() const
+			{
+				size_type tmp = sizeof(value_type);
+				tmp = (tmp == 1) ? 2 : tmp;
+
+				return (std::numeric_limits<size_type>::max() / tmp);
+			}
+
+			size_type	size() const
+			{
+				return (this->_size);
+			}
+
+			size_type	capacity() const
+			{
+				return (this->_capacity);
+			}
 
 		private :
 
-			Alloc	_alloc;
-			T	* _type;
+			pointer	_data;
+			allocator_type	_alloc;
+			size_type	_capacity;
+			size_type	_size;
 
 	};
 };

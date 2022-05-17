d55ab985515f2ad604922c452e59877938234ebb
alois <aloisebrechts@gmail.com>
2022-05-17 18:41:52 +0200

    insert

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: asebrech <asebrech@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 13:00:22 by asebrech          #+#    #+#             */
-/*   Updated: 2022/05/16 11:04:04 by asebrech         ###   ########.fr       */
+/*   Updated: 2022/05/17 18:11:42 by asebrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,74 +20,46 @@
 
 void	test()
 {
-	ft::vector<int>	vector(4, 117);
-	ft::vector<int>	vector2(4, 118);
-
-
-	ft::vector<int>	ftvector(vector);
-
-	ftvector.swap(vector2);
+	ft::vector<int>	vector(5, 117);
+	std::vector<int>	vec(5, 119);
 
 	ft::vector<int>::iterator	it;
 
-//	for (it = ftvector.begin(); it != ftvector.end(); it++)
-//		std::cout << "ft " << *it << std::endl;
+	//vector.reserve(11);	
 
-	std::cout << vector.max_size() << std::endl;
+	//std::cout << "pos " << *vector.insert(vector.begin() + 2, 118) << std::endl;
+	vector.insert((vector.begin() + 2), 2, 118);
+	//vector.insert((vector.begin() + 2), vec.begin(), vec.end());
+	std::cout << "cap " << vector.capacity() << std::endl;
+	std::cout << "siz " << vector.size() << std::endl;
 
-	ft::vector<int>::reference a = vector.at(3);
-
-//	a = 12;
-
-	std::cout << "operator [] " << a << std::endl;
-	std::cout << " " << vector.back() << std::endl;
-
-	vector.assign(2,42);
-
-	it = vector.begin();
-	std::cout << *vector.insert(vector.end(), 5) << std::endl;
-	vector.push_back(1337);
 	for (it = vector.begin(); it != vector.end(); it++)
 		std::cout << "ft " << *it << std::endl;
 }
 
 void	test2()
 {
-	std::vector<int>	vector(4, 117);
-	std::vector<int>	vector2(4, 118);
-
-
-	std::vector<int>	ftvector(vector);
-
-	ftvector.swap(vector2);
+	std::vector<int>	vector(5, 117);
+	std::vector<int>	vec(5, 119);
 
 	std::vector<int>::iterator	it;
 
-//	for (it = ftvector.begin(); it != ftvector.end(); it++)
-//		std::cout << "std " << *it << std::endl;
+	//vector.reserve(11);	
 
-	std::cout << vector.max_size() << std::endl;
+	//std::cout << "pos " << *vector.insert((vector.begin() + 2), 8) << std::endl;
+	vector.insert((vector.begin() + 2), 2, 118);
+	//vector.insert((vector.begin() + 2), vec.begin(), vec.end());
+	std::cout << "cap " << vector.capacity() << std::endl;
+	std::cout << "siz " << vector.size() << std::endl;
 
-	std::vector<int>::reference a = vector.at(3);
-
-//	a = 12;
-
-	std::cout << "operator [] " << a << std::endl;
-	std::cout << " " << vector.back() << std::endl;
-
-	vector.assign(2,42);
-
-	it = vector.begin();
-	std::cout << *vector.insert(vector.end(), 5) << std::endl;
-	vector.push_back(1337);
 	for (it = vector.begin(); it != vector.end(); it++)
 		std::cout << "std " << *it << std::endl;
-
 }
 
 int	main()
 {
 	test();
+	std::cout << std::endl;
 	test2();
 
 	return 0;	
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: asebrech <asebrech@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 12:51:49 by asebrech          #+#    #+#             */
-/*   Updated: 2022/05/16 11:03:17 by asebrech         ###   ########.fr       */
+/*   Updated: 2022/05/17 18:37:02 by asebrech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,9 +108,44 @@ namespace	ft
 
 			size_type	max_size() const { return (_alloc.max_size()); }
 
+			void	resize(size_type n, value_type val = value_type())
+			{
+				if (n < _size)
+				{
+					for(size_type i = _size; i >= n; i--)
+						_alloc.destroy(_data + i);
+					_size = n;
+				}
+				else if (n > _size)
+				{
+					reserve(n);
+					for (size_type i = _size; i < n; i++)
+						_alloc.construct(_data + i, val);
+					_size = n;
+				}
+			}
+
 			size_type	capacity() const { return (_capacity); }
 
 			bool	empty() const { return (_size == 0); }
+
+			void	reserve(size_type n)
+			{
+				if (n > _capacity)
+				{
+					if (n > max_size())
+						throw	std::length_error("vector");
+					pointer	tmp = _alloc.allocate(n); 
+					for(size_type i = 0; i < _size; i++)
+					{
+						_alloc.construct(tmp + i, _data[i]);
+						_alloc.destroy(_data + i);
+					}
+					_alloc.deallocate(_data, _capacity);	
+					_capacity = n;
+					_data = tmp;
+				}
+			}
 
 			/*	Elements access		*/
 
@@ -143,11 +178,12 @@ namespace	ft
 			void	assign(InputIterator first, InputIterator last,
 					typename ft::enable_if<!ft::is_integral<InputIterator>::value, bool>::type = false)
 			{
-				clear();
+				size_type	n = 0;
 				for (InputIterator it = first; it != last; it++)
-					_size++;
-				if (_size > _capacity)
-					_new_allocate();
+					n++;
+				clear();
+				reserve(n);
+				_size = n;
 				for (size_type i = 0; i < _size; i++)
 					_alloc.construct((_data + i), *first++);
 			}
@@ -155,68 +191,110 @@ namespace	ft
 			void	assign(size_type n, value_type const & val)
 			{
 				clear();
+				reserve(n);
 				_size = n;
-				if (_size > _capacity)
-					_new_allocate();
 				for (size_type i = 0; i < _size; i++)
 					_alloc.construct((_data + i), val);
 			}
 
-			void	push_back(value_type const & val) { insert(end(), val); } 
+			void	push_back(value_type const & val) { insert(end(), 1, val); } 
 			
 			// single element
 			iterator	insert(iterator position, value_type const & val)
 			{
-				size_type i = 0;
-				size_type rtn = 0;
-				if(_size + 1 > _capacity)
-				{
-					_size++;
-					if (_size > max_size())
-						throw	std::length_error("vector");
-					pointer	tmp = _alloc.allocate(_size); 
-					iterator	it = begin();
-					while(it != position)
-					{
-						tmp[i] = *it;
-						i++;
-						it++;
-					}
-					rtn = i;
-					tmp[i] = val;
+				size_type	i = 0;
+				for (iterator it = begin(); it != position; it++)
 					i++;
-					while(it != end())
-					{
-						tmp[i] = *it;
-						i++;
-						it++;
-					}
-					clear();
+				insert(position, 1, val);
+				return (begin() + i);
+			}
+
+			// fill	
+			void	insert(iterator position, size_type n, value_type const & val)
+			{
+				if((_size + n) > _capacity)
+				{
+					size_type	rsize = _capacity * 2;
+					if (rsize < (_size + n))
+						rsize = (_size + n);
+					if (rsize > max_size())
+						throw	std::length_error("vector");
+					pointer	tmp = _alloc.allocate(rsize); 
+					iterator	it;
+					size_type i = 0;
+					for (it = begin(); it != position; it++)
+						_alloc.construct(tmp + i++, *it);
+					for (size_type j = 0; j < n; j++)
+						_alloc.construct(tmp + i++, val);
+					while (it != end())
+						_alloc.construct(tmp + i++, *it++);
+					for (i = 0; i < _size; i++)
+						_alloc.destroy(_data + i);
 					_alloc.deallocate(_data, _capacity);
-					_capacity++;
-					_size = _capacity;
+					_capacity = rsize;
+					_size += n;
 					_data = tmp;
 				}
 				else
 				{
+					size_type	i = 0;
 					for (iterator it = begin(); it != position; it++)
 						i++;
-					value_type	tmp = _data[i];
-					value_type	tmp2;
-					_data[i] = val;
-					rtn = i;
-					i++;
-					while(i < _size)
+					for (size_type j = i; j < _size; j++)
+						_alloc.construct((_data + (j + n)), _data[j]);
+					for (size_type j = 0; j < n; j++)
 					{
-						tmp2 = _data[i];
-						_data[i] = tmp;
-						tmp = tmp2;
-						i++;
+						_alloc.destroy(_data + i);
+						_alloc.construct(_data + i++, val);
 					}
-					_data[i] = tmp;
-					_size++;
+					_size += n;
 				}
-				return (begin() + rtn);
+			}
+
+			template <class InputIterator>
+			void	insert(iterator position, InputIterator first, InputIterator last,
+					typename ft::enable_if<!ft::is_integral<InputIterator>::value, bool>::type = false)
+			{
+				int	n = 0;
+				for (InputIterator it = first; it != last; it++)
+					n++;
+				if((_size + n) > _capacity)
+				{
+					size_type	rsize = _capacity * 2;
+					if (rsize < (_size + n))
+						rsize = (_size + n);
+					if (rsize > max_size())
+						throw	std::length_error("vector");
+					pointer	tmp = _alloc.allocate(rsize); 
+					iterator	it;
+					size_type i = 0;
+					for (it = begin(); it != position; it++)
+						_alloc.construct(tmp + i++, *it);
+					while (first != last)
+						_alloc.construct(tmp + i++, *first++);
+					while (it != end())
+						_alloc.construct(tmp + i++, *it++);
+					for (i = 0; i < _size; i++)
+						_alloc.destroy(_data + i);
+					_alloc.deallocate(_data, _capacity);
+					_capacity = rsize;
+					_size += n;
+					_data = tmp;
+				}
+				else
+				{
+					size_type	i = 0;
+					for (iterator it = begin(); it != position; it++)
+						i++;
+					for (size_type j = i; j < _size; j++)
+						_alloc.construct((_data + (j + n)), _data[j]);
+					while (first != last)
+					{
+						_alloc.destroy(_data + i);
+						_alloc.construct(_data + i++, *first++);
+					}
+					_size += n;
+				}
 			}
 
 			void	swap(vector & x)
@@ -245,15 +323,6 @@ namespace	ft
 				error.append(ft::to_string(_size));
 				error.push_back(')');
 				throw	std::out_of_range(error);
-			}
-
-			void	_new_allocate()
-			{
-				_alloc.deallocate(_data, _capacity);
-				_capacity = _size;
-				if (_capacity > max_size())
-					throw	std::length_error("vector");
-				_data = _alloc.allocate(_capacity);
 			}
 	};
 };
